ed3e1f828ae6c256c607c559105b220184d9875c
ornenovino <istar.novino@gmail.com>
2023-09-10 17:31:41 -0300

    update del css

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -7,7 +7,7 @@ const Card = ({ user }) => {
   const { name, username, id } = user;
   const addFav = () => {
     const existingFavs = JSON.parse(localStorage.getItem('favUsers'));
-    if(existingFavs?.find(fav => fav.id === user.id)){
+    if (existingFavs?.find(fav => fav.id === user.id)) {
       return;
     }
     localStorage.setItem('favUsers', JSON.stringify(existingFavs ? [...existingFavs, user] : [user]))
@@ -15,16 +15,10 @@ const Card = ({ user }) => {
   }
 
   return (
-    <div className="card">
-      {/* En cada card deberan mostrar en name - username y el id */}
-
-      {/* No debes olvidar que la Card a su vez servira como Link hacia la pagina de detalle */}
-
-      {/* Ademas deberan integrar la logica para guardar cada Card en el localStorage */}
-
+    <div className="card bg-cyan-50 rounded-lg flex-row gap-2 p-5">
       <Link to={`dentist/${id}`}>{name}</Link>
       <p>{username}</p>
-      <button onClick={addFav} className="favButton">Add fav</button>
+      <button onClick={addFav} className="favButton rounded-lg">Add fav</button>
     </div>
   );
 };
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -10,14 +10,23 @@ const Navbar = () => {
   const { theme, handleChangeTheme } = useContext(ThemeContext);
 
   return (
-    <div style={{ backgroundColor: theme.backgroundColor, color: theme.color }}>
-      <nav>
-        <ul>
+    <div className="h-[100vh]" style={{ backgroundColor: theme.backgroundColor, color: theme.color }}>
+      <nav className='flex w-full justify-end'>
+        <ul className='flex'>
           <li><Link to="/" >Home</Link></li>
           <li><Link to="/contact" >Contact</Link></li>
           <li><Link to="/favs" >Favs</Link></li>
         </ul>
-        <button onClick={handleChangeTheme}>Change theme</button>
+        <button onClick={handleChangeTheme}>
+          <svg xmlns="http://www.w3.org/2000/svg" xmlns: xlink="http://www.w3.org/1999/xlink" version="1.1" width="256" height="256" viewBox="0 0 256 256" xml: space="preserve">
+
+            <defs>
+            </defs>
+            <g style="stroke: none; stroke-width: 0; stroke-dasharray: none; stroke-linecap: butt; stroke-linejoin: miter; stroke-miterlimit: 10; fill: none; fill-rule: nonzero; opacity: 1;" transform="translate(1.4065934065934016 1.4065934065934016) scale(2.81 2.81)" >
+              <path d="M 69.491 71.08 c -18.67 3.414 -36.573 -8.953 -39.988 -27.623 S 38.456 6.884 57.126 3.469 c 1.777 -0.325 3.541 -0.463 5.292 -0.511 C 54.979 0.112 46.69 -0.796 38.282 0.742 C 13.839 5.212 -2.353 28.651 2.117 53.094 s 27.909 40.634 52.352 36.164 c 16.252 -2.972 28.847 -14.334 34.155 -28.798 C 83.739 65.803 77.161 69.677 69.491 71.08 z" style="stroke: none; stroke-width: 1; stroke-dasharray: none; stroke-linecap: butt; stroke-linejoin: miter; stroke-miterlimit: 10; fill: rgb(247,215,127); fill-rule: nonzero; opacity: 1;" transform=" matrix(1 0 0 1 0 0) " stroke-linecap="round" />
+            </g>
+          </svg>
+        </button>
       </nav>
       <Outlet />
       <Footer />
