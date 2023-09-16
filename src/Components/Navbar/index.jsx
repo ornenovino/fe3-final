cfede4c5ca7daf448cf539dab51a0657b4d0e651
ornenovino <istar.novino@gmail.com>
2023-09-16 05:08:27 -0300

    last changes

--- src/Components/Navbar/index.jsx
+++ src/Components/Navbar/index.jsx
@@ -19,7 +19,7 @@ const Navbar = () => {
               <li><Link to="/contact" >Contact</Link></li>
               <li><Link to="/favs" >Favs</Link></li>
             </ul>
-            <button onClick={handleChangeTheme}><svg className="w-5 h-5 text-gray-800 dark:text-white bg-color-black" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="#b89c2c" viewBox="0 0 18 20">
+            <button onClick={handleChangeTheme}><svg className="moon" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="#b89c2c">
               <path d="M17.8 13.75a1 1 0 0 0-.859-.5A7.488 7.488 0 0 1 10.52 2a1 1 0 0 0 0-.969A1.035 1.035 0 0 0 9.687.5h-.113a9.5 9.5 0 1 0 8.222 14.247 1 1 0 0 0 .004-.997Z" />
             </svg></button>
           </div>
